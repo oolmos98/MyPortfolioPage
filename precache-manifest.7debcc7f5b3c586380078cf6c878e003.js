9ca808960b2e67c3cb6b6d955417d1d2c8233ef8
Omar Olmos <oolmos@ucmerced.edu>
2022-02-05 09:48:40 -0700

    Updates

---
+++ precache-manifest.7debcc7f5b3c586380078cf6c878e003.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d08aa50d235d3755b24d4f7bbb4994bd",
+    "url": "/MyPortfolioPage/index.html"
+  },
+  {
+    "revision": "319410df29f1e14e27bb",
+    "url": "/MyPortfolioPage/static/css/main.f04de633.chunk.css"
+  },
+  {
+    "revision": "5caeaae02598237feb23",
+    "url": "/MyPortfolioPage/static/js/2.ea490775.chunk.js"
+  },
+  {
+    "revision": "319410df29f1e14e27bb",
+    "url": "/MyPortfolioPage/static/js/main.42aac727.chunk.js"
+  },
+  {
+    "revision": "494e679f1389766318db",
+    "url": "/MyPortfolioPage/static/js/runtime~main.937a1234.js"
+  },
+  {
+    "revision": "afcfabca20fdb4538095204eeffde50d",
+    "url": "/MyPortfolioPage/static/media/portfolioSelfie.afcfabca.png"
+  }
+]);
